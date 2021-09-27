6b8cf6172aa6239d6bf8f28c933b31a7d17b6c60
Negirox <mukeshsingh.negi007@gmail.com>
2021-09-27 09:52:45 +0530

    authentication added

--- ByohForSure/Controllers/UserLoginController.cs
+++ ByohForSure/Controllers/UserLoginController.cs
@@ -3,12 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using ByohForSure.Models;
+using ByohForSure.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ByohForSure.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class UserLoginController : Controller
     {
+        private IUserService _userService;
+
+        public UserLoginController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate(AuthenticateRequest model)
+        {
+            var response = _userService.Authenticate(model);
+
+            if (response == null)
+                return BadRequest(new { message = "Username or password is incorrect" });
+
+            return Ok(response);
+        }
+
+        [Authorize]
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            return Ok(users);
+        }
         public IActionResult Index()
         {
             return View();
